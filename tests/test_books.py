6a49622a49e25b8862e1a25186f4f52c9d424c4d
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-12-19 14:24:59 +0530

    Update test_books.py

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,1 +1,9 @@
+import unittest
+from app.models.book import book_model
+
+class TestBookModel(unittest.TestCase):
+    def test_create_book(self):
+        book = book_model.create({"title": "1984", "author": "George Orwell"})
+        self.assertEqual(book["title"], "1984")
+        self.assertEqual(book["author"], "George Orwell")
 
