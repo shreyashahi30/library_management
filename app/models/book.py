92e70c211e53c7322cf104cfd4f6a3ce53310862
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-12-19 14:20:32 +0530

    Update book.py

--- app/models/book.py
+++ app/models/book.py
@@ -1,1 +1,13 @@
+from app.models.base_model import BaseModel
+
+class Book(BaseModel):
+    def search(self, title=None, author=None):
+        results = []
+        for book in self.data.values():
+            if (title and title.lower() in book['title'].lower()) or \
+               (author and author.lower() in book['author'].lower()):
+                results.append(book)
+        return results
+
+book_model = Book()
 
