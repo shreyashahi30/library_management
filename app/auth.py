7c5e21d59d5f1b4f146fd88cd489be04eec44c10
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-12-19 14:23:20 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,13 @@
+# app/auth.py
+from flask import request, jsonify
+
+TOKEN = "secure_token"
+
+def token_required(f):
+    def wrapper(*args, **kwargs):
+        token = request.headers.get("Authorization")
+        if token != TOKEN:
+            return jsonify({"error": "Unauthorized"}), 401
+        return f(*args, **kwargs)
+    return wrapper
 
